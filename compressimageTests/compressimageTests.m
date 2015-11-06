35cf3704ca4568428469f4d6d723fdcfb45fe950
姚隆康 <584379066@qq.com>
2015-11-06 13:43:16 +0800

    Initial Commit

---
+++ compressimageTests/compressimageTests.m
@@ -0,0 +1,39 @@
+//
+//  compressimageTests.m
+//  compressimageTests
+//
+//  Created by bioongroup on 15/11/6.
+//  Copyright © 2015年 ylk. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface compressimageTests : XCTestCase
+
+@end
+
+@implementation compressimageTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
